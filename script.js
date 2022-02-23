6eeeb2380d3a8e38d76b1f0560ced6ce5c4a60ec
Alapottra <alapottratrojon@gmail.com>
2022-02-23 12:16:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const getCountries = () =>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+};
+
+
+const displayCountries = data => {
+   const container =  document.querySelector('.container');
+   data.forEach(country => {
+       const divCountry = document.createElement('div');
+       divCountry.classList.add('country');
+       divCountry.innerHTML = `
+       <div> <img src="${country.flags.png}" alt="" ></div>
+       <h3>Country Name : ${country.name.common}</h3>
+       <p> Population : ${country.population} </p>
+       
+       ` ;
+       container.appendChild(divCountry);
+
+   })
+}
+
+
+getCountries();
+
+
+
+
+const searchField = document.getElementById('search');
+
+searchField.addEventListener('keyup', ()=>{
+    const countries = document.getElementsByClassName('country');
+    const value = searchField.value.toLowerCase();
+    
+
+for(country of countries) {
+    if(country.children[2].innerText.toLowerCase().includes(value)){
+        country.style.display = 'block';
+    }else{
+        country.style.display = 'none';
+    }
+}
+    
+     
+ 
+
+   
+});
